997878b8d88614726a11d21531dc8cffd99c9c33
Kero <kero.mena1@gmail.com>
2025-04-26 10:25:55 -0700

    added glm, stb and assets folder for shaders
    added an ignore for the build folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,85 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std; 
+#include <fstream>
+#include <filesystem>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow*, int, int);
+void processInput(GLFWwindow*);
+std::string loadShaderSrc(const char*);
 
-int main()
-{
-    if (!glfwInit())
-    {
-        cout << "Failed to initialize GLFW" << endl;
-        return -1;
-    }
+int main() {
+    std::cout << "RudeRacoon @_@\n"; 
+    std::cout << "Current working directory: " 
+    << std::filesystem::current_path() << "\n";
 
+    glfwInit();
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW\n";;
+        return -1;
+    }
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+# endif
     GLFWwindow* window;
     window = glfwCreateWindow(800, 600, "RudeRacoon", NULL, NULL);
-    if (window == NULL)
-    {
-        cout << "Failed to open GLFW window" << endl;
+    if (window == NULL) {
+        std::cout << "Failed to open GLFW window\n";
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        cout << "Failed to initialize GLAD" << endl;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD\n";
         return -1;
     }
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while(!glfwWindowShouldClose(window))
-    {
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+std::string loadShaderSrc(const char* filename) {
+    std::ifstream file;
+    std::stringstream buf;
+    
+    std::string ret = "";
+
+    file.open(filename);
+    if (file.is_open()) {
+        buf << file.rdbuf();
+        ret = buf.str();
+    } else {
+        std::cout << "could not open " << filename << "\n";
+    }
+    file.close();
+
+    return ret;
 }
